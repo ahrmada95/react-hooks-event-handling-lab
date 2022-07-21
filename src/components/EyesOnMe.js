f2047ef071411f43566a2820c49eaa54322181bd
Ahnaf Ahmad <ahrmada95@gmail.com>
2022-07-21 09:57:26 -0400

    commit tree c446a05bf142b84a3ab5a37f21b7e29f068620cd

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,11 @@
+import React from "react"
 // Code EyesOnMe Component Here
+const EyesOnMe = () => {
+    return (
+        <div>
+            <button onFocus={() => console.log("Good!")} onBlur={() => console.log("Hey! Eyes on me!")}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe
